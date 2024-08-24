208bd02ff48b6a6448b0c5d2982b130c2678e841
핑구 <58054397+sihoo67@users.noreply.github.com>
2024-08-24 19:53:34 +0900

    Create PinguApp.java

---
+++ apps/PinguApp.java
@@ -0,0 +1,6 @@
+// PinguApp.java
+public class PinguApp {
+    public static void main(String[] args) {
+        System.out.println("This is a simple Java application running on Pingu OS 64-bit.");
+    }
+}
